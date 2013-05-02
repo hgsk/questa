b490fecd5b777aa79aa251318f1ce133c33f9242
vagrant <vagrant@precise32.(none)>
2013-05-02 11:49:44 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Questa::Application.config.secret_token = '19db9a3518e8861e642c139644c33806aa61361e0a59b324eb93f97d8999904bf2d5d50654f9d32833b37e3f93702e1ae9d9de416d4aef33fe50c85016a66396'
